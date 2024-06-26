eb0ee58f43502d9b3094bc6a1397890a556e3e8f
spandana0528 <spandanan71@gmail.com>
2024-06-26 23:26:53 +0530

    Create gcd.C

---
+++ gcd.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int n1,n2,min,i,gcd;
+scanf("%d%d",&n1,&n2);
+if(n1<n2)
+    min=n1;
+else
+    min=n2;
+for(i=1,gcd=1;i<min;i++)
+{
+    if(n1%i==0 && n2%i==0)
+    {
+        gcd=i;
+    }
+}
+printf("%d",gcd);
+}
